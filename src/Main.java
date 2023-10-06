58f421f8b8903e6c1d161d3d4fe0d42297c76a42
omar <oouertani02@gmail.com>
2023-10-06 11:37:00 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int sum(int[] array) {
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
+    public double average(int[] array) {
+        int sum = sum(array);
+        return (double) sum / array.length;
+    }
+    public int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+
+    public boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+}
+
+
+
