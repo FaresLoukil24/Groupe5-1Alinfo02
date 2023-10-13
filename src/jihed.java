f50250153caa2c277d50c21b6c92dd5ba0e77c4e
jihed <jihed.bouabid@esprit.tn>
2023-10-13 11:13:58 +0100

    inverseyhuhu

--- src/jihed.java
+++ src/jihed.java
@@ -2,7 +2,7 @@ public class jihed {
 
 
 
-        public static String inverse(String chaine) {
+        public static String inverseyhuhu(String chaine) {
             String inverse = "";
             for (int i = chaine.length() - 1; i >= 0; i--) {
                 inverse += chaine.charAt(i);
